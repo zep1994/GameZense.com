8cf23969059d84675cb8fbf6c6e426881d057b53
zep1994 <snaxbags@protonmail.com>
2019-12-10 21:48:46 -0500

    update validations

--- gamezense/app/models/article.rb
+++ gamezense/app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base 
-    validates :title, presence: true 
-    validates :description, presence: true
+    validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+    validates :description, presence: true, length: { minimum: 30, maximum: 300000 }
 end  
