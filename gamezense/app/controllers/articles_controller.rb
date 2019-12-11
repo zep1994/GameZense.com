fe95cef2ccb5f1b01f7b15037f90e8da27469e4b
zep1994 <snaxbags@protonmail.com>
2019-12-11 11:41:00 -0500

    Setup Show Page

--- gamezense/app/controllers/articles_controller.rb
+++ gamezense/app/controllers/articles_controller.rb
@@ -8,8 +8,17 @@ class ArticlesController < ApplicationController
     end 
 
     def create 
-        @article = Article.new
-        @article.save 
+    @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @article = Article.find(params[:id])
     end
 
     private 
