dbd1ef5406d747e2b2ed455f6d9f3303a42d7cfc
zep1994 <snaxbags@protonmail.com>
2019-12-10 21:46:43 -0500

    add Article model and validations

--- gamezense/db/schema.rb
+++ gamezense/db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_12_11_005710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+  end
 
 end
