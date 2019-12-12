3dca8ed47f6bc60ce7b4898e4bb9003cfce3d5c5
zep1994 <snaxbags@protonmail.com>
2019-12-11 22:09:52 -0500

    update controller for user model

--- gamezense/app/models/article.rb
+++ gamezense/app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base 
     validates :title, presence: true, length: { minimum: 3, maximum: 50 }
     validates :description, presence: true, length: { minimum: 30, maximum: 300000 }
+    validates :user_id, presence: true
+    belongs_to :user
 end  
